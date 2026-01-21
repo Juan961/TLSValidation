0a4224e7a08d32d5495cd17030a6620e7da83335
Juan Jose <juanjo5792@gmail.com>
2026-01-21 18:54:31 -0500

    Add aws lambda and gw proxy integration

--- api/go.mod
+++ api/go.mod
@@ -8,4 +8,8 @@ require (
 	github.com/go-chi/render v1.0.3
 )
 
-require github.com/ajg/form v1.5.1 // indirect
+require (
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/aws/aws-lambda-go v1.52.0 // indirect
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2 // indirect
+)
--- api/main.go
+++ api/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"context"
 
 	"validator/routes"
 
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	chiadapter "github.com/awslabs/aws-lambda-go-api-proxy/chi"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 )
 
-func main() {
-	port := "3333"
+var chiLambdaV2 *chiadapter.ChiLambdaV2
 
+func setupRouter() *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
@@ -29,7 +31,19 @@ func main() {
 
 	r.Get("/validate", routes.ValidateRoute)
 
-	fmt.Println("Server running on port " + port)
+	return r
+}
 
-	http.ListenAndServe(":"+port, r)
+func init() {
+	r := setupRouter()
+	chiLambdaV2 = chiadapter.NewV2(r)
+	chiLambdaV2.StripBasePath("/prod")
+}
+
+func handler(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+	return chiLambdaV2.ProxyWithContextV2(ctx, req)
+}
+
+func main() {
+	lambda.Start(handler)
 }
