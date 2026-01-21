c384ca4ee85b9bbaab9e9d3a2d2a96c3e1678372
Juan Jose <juanjo5792@gmail.com>
2026-01-20 22:17:46 -0500

    Basic implementation of request

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const baseURL string = "https://api.ssllabs.com/api/v2/analyze?host=www.ssllabs.com"
+
+	resp, err := http.Get(baseURL)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(resp.StatusCode)
+
+	defer resp.Body.Close()
+	bodyBytes, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
+}
