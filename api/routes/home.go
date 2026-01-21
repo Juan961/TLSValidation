2adfd6c9e4eb6080417c9cb1fa22b8a2df8fe9bc
Juan Jose <juanjo5792@gmail.com>
2026-01-21 14:18:40 -0500

    Delete go native html template

--- api/routes/home.go
+++ api/routes/home.go
@@ -1,12 +1,12 @@
 package routes
 
 import (
-	"html/template"
 	"net/http"
+	"os"
 )
 
 func HomeRoute(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.html")
+	content, err := os.ReadFile("templates/index.html")
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -14,11 +14,5 @@ func HomeRoute(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	err = tmpl.Execute(w, nil)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	w.Write(content)
 }
